f916550d47ff3e4356658cc59e1c64995b702dcc
Po-Jui Chen <a00012025@gmail.com>
2017-05-30 22:40:30 +0800

    Add basic UI and event handler
    - token and result text
    - submit button and its onclick event handler

--- app/src/main/java/dsdl/coldwalletapp/MainActivity.java
+++ app/src/main/java/dsdl/coldwalletapp/MainActivity.java
@@ -2,6 +2,10 @@ package dsdl.coldwalletapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    protected void onSubmitClick(View v){
+        EditText token = (EditText)findViewById(R.id.Token);
+        String s = token.getText().toString();
+        // Send s via BLE
+
+        String res = "XDD";
+        TextView result = (TextView)findViewById(R.id.Result);
+        result.setText(res);
+    }
+
 }
